a848a0e83dd10b46891fb1d0398452e9e3fddb1d
Athailah, S.Kom <atha@atha.web.id>
2023-09-05 21:20:11 +0700

    Perubahana password db lagi

--- config/database.php
+++ config/database.php
@@ -57,7 +57,7 @@ return [
             'host'      => env('DB_HOST', 'localhost'),
             'database'  => env('DB_DATABASE', 'avcs'),
             'username'  => env('DB_USERNAME', 'root'),
-            'password'  => env('DB_PASSWORD', 'toor'),
+            'password'  => env('DB_PASSWORD', 'root123'),
             'charset'   => 'utf8',
             'collation' => 'utf8_unicode_ci',
             'prefix'    => '',
